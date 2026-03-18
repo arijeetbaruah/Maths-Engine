771ebca9ffeb68974193f94fe5f945c6384e15e8
arijeetbaruah <arijeet@tentworks.io>
2026-03-18 11:24:30 +0530

    documentation update

--- docs/files_dup.js
+++ docs/files_dup.js
@@ -1,5 +1,5 @@
 var files_dup =
 [
-    [ "docs~", "dir_79806bf1b2116a0265185ece42e3c42c.html", "dir_79806bf1b2116a0265185ece42e3c42c" ],
+    [ "docs", "dir_49e56c817e5e54854c35e136979f97ca.html", "dir_49e56c817e5e54854c35e136979f97ca" ],
     [ "Packages", "dir_395bafe2adc24fcfed5d14b8ce406a4f.html", "dir_395bafe2adc24fcfed5d14b8ce406a4f" ]
 ];
